38bdf0c437c4232493bc4274ecf066421d21c745
Swapnil5390 <pawar3382@gmail.com>
2025-02-15 11:54:36 +0530

    Add project files.

---
+++ URLShortner/Controllers/URLController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace URLShortner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class URLController : ControllerBase
+    {
+
+
+
+    }
+}
